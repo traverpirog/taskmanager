793d7f521ca47ba9b8db26ef21efb7c3b4c8ce62
Igor <traver.unimmer@gmail.com>
2023-11-25 16:19:18 +0300

    init: start taskmanager project

---
+++ src/test/java/ru/travorpirog/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.travorpirog.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
